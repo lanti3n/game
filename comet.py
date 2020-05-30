12f3998e7fcb889b9ae544e5050e7bf75905b972
l@nti3n <lantien@elsewhere.com>
2020-05-30 19:43:16 +0200

    adding comet fontion down

--- comet.py
+++ comet.py
@@ -8,19 +8,17 @@ class Comet(pygame.sprite.Sprite):
         #appelle de la classe sprite pour la charger lors de la creation de la Comet
         super().__init__()
         self.game = game
-        self.attack = 0.3
+        self.attack = 10
         self.image = pygame.image.load('assets/comet.png')
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, 1000)
         self.rect.y = 0
         self.velocity = random.randint(1,3)
 
-
     def down(self):
         #le deplacement se fait que s il n'y a pas de collision avec un group de joueur
         if not self.game.check_collision(self, self.game.all_players):
             self.rect.y += self.velocity
-            print('çolision')
         #si la comet est en collision avec le joueur
         else:
             #ingliger des degats au joueur
@@ -29,3 +27,12 @@ class Comet(pygame.sprite.Sprite):
             self.rect.x = random.randint(0, 1000)
             self.rect.y = 0
             self.velocity = random.randint(1,3)
+        
+        #print(self.rect.y)
+
+        #verifier si la comet n'est plus present a l ecran
+        if self.rect.y >= 500:
+           #reinitialiser le projectile
+           self.rect.x = random.randint(0, 1000)
+           self.rect.y = 0
+           print('comet reinit !')
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ banner_rect.x = math.ceil(screen.get_width() / 4)
 
 # importer/charger le bouton pour lancer la partie
 play_button = pygame.image.load('assets/button.png')
-play_button = pygame.transform.scale(play_button, (400, 150))
+play_button = pygame.transform.scale(play_button, (400, 150))  
 play_button_rect = play_button.get_rect()
 play_button_rect.x = math.ceil(screen.get_width() / 3.33)
 play_button_rect.y = math.ceil(screen.get_height() / 2)
@@ -31,7 +31,7 @@ click = pygame.mixer.Sound('assets/sounds/click.ogg')
  
 # charger notre jeu
 game = Game()
-
+   
 running = True
 
 
